f801a09896bce3a99303e2d3c6b0fa23d3b40842
hjm <gks777777@gmail.com>
2024-01-17 16:22:44 +0900

    Spring Security, JWT 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,13 +31,17 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-aop") //Spring AOP
+    implementation("org.springframework.boot:spring-boot-starter-aop") //Spring AOPN
+    implementation("org.springframework.boot:spring-boot-starter-security") //Spring Security
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3") //JWT
     // implementation("com.h2database:h2")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 }
 
 tasks.withType<KotlinCompile> {

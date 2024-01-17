fd51e9fee73b1e60dded8a0ea127804335ed8033
hjm <gks777777@gmail.com>
2024-01-17 12:19:39 +0900

    TestAop 주석처리

--- src/main/kotlin/com/teamsparta/courseregistration/infra/aop/TestAop.kt
+++ src/main/kotlin/com/teamsparta/courseregistration/infra/aop/TestAop.kt
@@ -5,8 +5,8 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.springframework.stereotype.Component
 
-@Aspect
-@Component
+//@Aspect
+//@Component
 class TestAop {
 
     @Around("execution(* com.teamsparta.courseregistration.domain.course.service.CourseService.getCourseById(..))")
